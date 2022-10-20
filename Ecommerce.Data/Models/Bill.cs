0b63be39ce41a9a609c3f575773fc678710f5e36
Francisco Zalazar <franz.zalazar@gmail.com>
2022-10-20 20:25:19 -0300

    Add some atributes to Bill model and DTO

--- Ecommerce.Data/Models/Bill.cs
+++ Ecommerce.Data/Models/Bill.cs
@@ -13,6 +13,9 @@ namespace Ecommerce.Data.Models
         public int ID_Bill { get; set; }
         public int ID_BillDetail { get; set; }
         public int ID_Client { get; set; }
+        public string date { get; set; }
+        public double total { get; set; }
+        public int BillNumber { get; set; }
 
         public List<Bill> BillList { get; set; } = new List<Bill>();
     }
--- Ecommerce.Domain/DTO/BillDTO.cs
+++ Ecommerce.Domain/DTO/BillDTO.cs
@@ -12,6 +12,9 @@ namespace Ecommerce.Domain.DTO
         public int ID_BillDetail { get; set; }
         public int ID_Client { get; set; }
         public int ID_BillDetails { get; set; }
+         public string date { get; set; }
+        public double total { get; set; }
+        public int BillNumber { get; set; }
 
     }
 }
